a28b3a2b27a9694a20a83abdad431bec55b3a416
Sharky88888888 <159481506+Sharky88888888@users.noreply.github.com>
2025-10-31 14:55:35 -0400

    Add Transaction class

--- Document.java
+++ Document.java
@@ -1,20 +1,42 @@
-import java.nio.file.*;
-import java.io.IOException;
+import java.time.LocalDate;
 
-public class Document {
-    private String filename;
-    private String content;
+public class Transaction {
+    private String type;
+    private String category;
+    private double amount;
+    private LocalDate date;
+    private String description;
 
-    public Document(String filename) throws IOException {
-        this.filename = filename;
-        content = new String(Files.readAllBytes(Paths.get(filename))).toLowerCase();
+    public Transaction(String type, String category, double amount, LocalDate date, String description) {
+        this.type = type;
+        this.category = category;
+        this.amount = amount;
+        this.date = date;
+        this.description = description;
     }
 
-    public String getFilename() {
-      return filename; 
+    public String getType() { 
+        return type; 
     }
-  
-    public String getContent() { 
-      return content; 
+    
+    public String getCategory() {
+        return category; 
+    }
+    
+    public double getAmount() {
+        return amount; 
+    }
+    
+    public LocalDate getDate() {
+        return date; 
+    }
+    
+    public String getDescription() {
+        return description; 
+    }
+
+    @Override
+    public String toString() {
+        return date + " | " + type + " | " + category + " | $" + amount + " | " + description;
     }
 }
